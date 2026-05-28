b4121149ac5a52875e30a27ce5ed1089b26e97b1
markymelaj <sistemas@luminartchile.com>
2026-05-28 14:06:13 -0400

    Update config.js

--- config.js
+++ config.js
@@ -19,6 +19,7 @@ window.BELLAVISTA_CONFIG = {
 
 window.bv = window.supabase.createClient(
 SUPABASE_URL:  'https://wtqxyclosajruswjjcbm.supabase.co',
-  SUPABASE_ANON: 'YOUR-ANON-KEY-HERE',
+  SUPABASE_ANON: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0cXh5Y2xvc2FqcnVzd2pqY2JtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3OTg2NTQsImV4cCI6MjA5NTM3NDY1NH0.wsy3gHEQ1Mu3_rXlNHG8rD6PYxTZxRDkkDJlYR53JM4',
+  WHATSAPP:      '56974984220',
   { auth: { persistSession: true, autoRefreshToken: true } }
 );
